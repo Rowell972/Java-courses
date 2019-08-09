f66cef4125897a180c94d342a0337ffb9a4f3db4
Roman Kovalev <kovalevroman1997@gmail.com>
2019-08-09 20:14:51 +0500

    calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,21 +1,32 @@
-
 public class Calculator {
-	
+
 	private int result = 0;
-	
-	
+
+
 	public void add(int ... params) {
 		for	(Integer param : params) {
 			result += param;
 		}
 	}
 	
+	public void sub(int ... params) {
+		result = params[0] - params[1];
+	}
 	
+	public void mult(int ... params) {
+		result = params[0] * params[1];
+	}
+	
+	public void div(int ... params) {
+		result = params[0] / params[1];
+	}
+
+
 	public int getResult() {
 		return this.result;
 	}
-	
-	
+
+
 	public void cleanResult() {
 		this.result = 0;
 	}
--- src/InteractRunner.java
+++ src/InteractRunner.java
@@ -2,22 +2,51 @@ import java.util.Scanner;
 
 
 public class InteractRunner {
-	
+
 	public static void main(String[] args) {
 		Scanner scaner = new Scanner(System.in);
-		
+
 		try {
 			Calculator calc = new Calculator();
+			System.out.println("Enter first argument: ");
+			String first = scaner.next();
 			String exit = "no";
+			String cleanResult = "no";
 			while(!exit.equals("yes")) {
-				System.out.println("Enter first argument: ");
-				String first = scaner.next();
 				System.out.println("Enter second argument: ");
 				String second = scaner.next();
-				calc.add(Integer.parseInt(first), Integer.parseInt(second));
-				System.out.println("Result: " + calc.getResult());
-				calc.cleanResult();
-				System.out.println("Exit: yes/no");
+				System.out.println("Enter operand: ");
+				String answer = scaner.next();
+				switch (answer) {
+				case ("+") :
+					calc.add(Integer.parseInt(first), 
+							Integer.parseInt(second));
+					System.out.println("Result: " + calc.getResult());
+					break;
+				case ("-") :
+					calc.sub(Integer.parseInt(first), 
+							Integer.parseInt(second));
+					System.out.println("Result: " + calc.getResult());
+					break;
+				case ("*") :
+					calc.mult(Integer.parseInt(first), 
+							Integer.parseInt(second));
+					System.out.println("Result: " + calc.getResult());
+					break;
+				case ("/") :
+					calc.div(Integer.parseInt(first), 
+							Integer.parseInt(second));
+					System.out.println("Result: " + calc.getResult());
+					break;
+				}
+				System.out.println("Clean result? yes/no");
+				cleanResult = scaner.next();
+				if (cleanResult.equals("yes")) {
+					calc.cleanResult();
+					System.out.println("Enter first argument: ");
+					first = scaner.next();
+				}
+				System.out.println("Exit? yes/no");
 				exit = scaner.next();
 			}
 		} finally {
